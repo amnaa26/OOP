51c707ee2b0c5b3e54f0b08904ee098ccf607434
Amna <142903458+amnaa26@users.noreply.github.com>
2024-02-11 20:42:32 +0500

    Updated header

--- Labs/02/q5.cpp
+++ Labs/02/q5.cpp
@@ -1,12 +1,9 @@
 /*
-	Programmer: Amna(23K-0066)
-	Question: Your assignment is to construct a basic product management system for an online store.
-			  1. Develop a function that enables the addition of a new product to the system. The function should accept parameters 
-			  	 such as product name, price, quantity, and any other relevant details.
-			  2. Implement a function that takes a product ID as input and displays detailed information about the product, including 
-			  	 its name, price, quantity in stock, and any other relevant details.
-			  3. Design a function that allows the update of product information. It should accept a product ID and the new details 
-			  	 (e.g., updated price, quantity, etc.) and modify the existing product’s information accordingly.
+Programmer: Amna(23K-0066)
+Question: Your assignment is to construct a basic product management system for an online store.
+	  1. Develop a function that enables the addition of a new product to the system. The function should accept parameters such as product name, price, quantity, and any other relevant details.
+	  2. Implement a function that takes a product ID as input and displays detailed information about the product, including its name, price, quantity in stock, and any other relevant details.
+	  3. Design a function that allows the update of product information. It should accept a product ID and the new details (e.g., updated price, quantity, etc.) and modify the existing productâ€™s information accordingly.
 */
 
 #include <iostream>
