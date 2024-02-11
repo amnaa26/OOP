2771fb35b5b7da9d24aeeab9581a4ea81fde6f47
Amna <142903458+amnaa26@users.noreply.github.com>
2024-02-11 20:34:34 +0500

    Update q5.cpp

--- Labs/01/q5.cpp
+++ Labs/01/q5.cpp
@@ -1,18 +1,15 @@
 /*
-	
-	Programmer: Amna(23K-0066)
-
+Programmer: Amna(23K-0066)
 Q5: Design Event Management System in C++ to facilitate the organization of events. The program should empower the user to seamlessly manage event details, including the 
-	event name, date, venue, and organizer. Incorporate key features such as the ability to add events, display comprehensive details of all events, and perform event 
-	searches based on specific dates. To enhance flexibility and scalability, leverage dynamic memory allocation for storing event details.
-	
+    event name, date, venue, and organizer. Incorporate key features such as the ability to add events, display comprehensive details of all events, and perform event 
+    searches based on specific dates. To enhance flexibility and scalability, leverage dynamic memory allocation for storing event details.
 	1. Prompt the user to input the total number of events they wish to manage.
 	2. Dynamically allocate memory to store event details for the specified number of events.
 	3. For each event, prompt the user to input:
-		• Event Name
-		• Date
-		• Venue
-		• Organizer
+		â€¢ Event Name
+		â€¢ Date
+		â€¢ Venue
+		â€¢ Organizer
 	4. Display all events that match the provided date, including their complete details.
 	5. Allow the user to search for events based on a specific date.
 	
