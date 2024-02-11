22fe2c0575078ca6f55cc119d8a5c965a5f1aaa8
Amna <142903458+amnaa26@users.noreply.github.com>
2024-02-11 20:34:04 +0500

    Update q4.cpp

--- Labs/01/q4.cpp
+++ Labs/01/q4.cpp
@@ -1,10 +1,8 @@
 /*
-
-	Programmer: Amna(23K-0066)
-	
+Programmer: Amna(23K-0066)
 Q4: You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-	Find two lines that together with the x-axis form a container, such that the container contains the most water. Return the maximum amount of water a container can store. 
-	Notice that you may not slant the container.
+    Find two lines that together with the x-axis form a container, such that the container contains the most water. Return the maximum amount of water a container can store. 
+    Notice that you may not slant the container.
 */
 
 
