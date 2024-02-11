8c9fabacc9da8b18eb11513533e1cacc668f5bea
Amna <142903458+amnaa26@users.noreply.github.com>
2024-02-11 20:39:34 +0500

    Updated header

--- Labs/02/q4.cpp
+++ Labs/02/q4.cpp
@@ -1,14 +1,13 @@
 /*
-	Programmer: Amna(23K-0066)
-	Question: Your task is to implement a basic Student Registration System in C++. Define two structures, Register and Student, 
-			  where Register includes attributes courseId and courseName, and Student inherits from Register while having additional 
-			  attributes such as studentId, firstName, lastName, cellNo, and email. Your objective is to create an array of Student structures 
-			  to store data for five students. Write a C++ program that accomplishes the following tasks:
-				1. Implement the Register and Student structures.
-				2. Inherit the Register structure in the Student structure.
-				3. Create an array of Student structures to store data for 5 students.
-				4. Take input for each student, including their courseId, courseName, studentId, firstName, lastName, cellNo, and email.
-				5. Display the information for all 5 students.
+Programmer: Amna(23K-0066)
+Question: Your task is to implement a basic Student Registration System in C++. Define two structures, Register and Student, where Register includes attributes courseId and courseName, 
+	   and Student inherits from Register while having additional attributes such as studentId, firstName, lastName, cellNo, and email. Your objective is to create an array of Student 
+    	   structures to store data for five students. Write a C++ program that accomplishes the following tasks:
+		1. Implement the Register and Student structures.
+		2. Inherit the Register structure in the Student structure.
+		3. Create an array of Student structures to store data for 5 students.
+		4. Take input for each student, including their courseId, courseName, studentId, firstName, lastName, cellNo, and email.
+		5. Display the information for all 5 students.
 */
 
 #include <iostream>
