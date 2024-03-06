5099dbbc81ddf7bd92f2531b2ebf03330dfa40db
Amna <142903458+amnaa26@users.noreply.github.com>
2024-03-07 00:55:07 +0500

    Updated header

--- Labs/05/q4.cpp
+++ Labs/05/q4.cpp
@@ -2,21 +2,21 @@
  * Creator: Amna(23k-0066)
  * Desc: Create a BankAccount class. Which contains following details and functionalties:
 		 Attributes: The BankAccount class has three private member variables: accountNumber, accountHolderName, and balance.
-		 Create Constructor: The class has a constructor that takes parameters to initialize the account details (accountNumber, accountHolderName, and balance).
-		 Create following Member Functions:
-				• deposit(double amount): Adds the specified amount to the account balance.
-				• withdraw(double amount): Subtracts the specified amount from the account balance, if sufficient funds are available.
-				• display(): Displays the account details including the account number, account holder name, and balance.
+	 Create Constructor: The class has a constructor that takes parameters to initialize the account details (accountNumber, accountHolderName, and balance).
+	 Create following Member Functions:
+			â€¢ deposit(double amount): Adds the specified amount to the account balance.
+			â€¢ withdraw(double amount): Subtracts the specified amount from the account balance, if sufficient funds are available.
+			â€¢ display(): Displays the account details including the account number, account holder name, and balance.
 
-		 In the main() function, create an array accounts of BankAccount objects. The array contains three elements, each representing a different bank account.
-		 Initialize Each BankAccount object with specific account details such as account number, holder name, and initial balance.
-		 Perform following operations:
-				• Iterate through each account in the accounts array.
-				• For each account, display the account details using the display() function.
-				• Perform Two Transactions:
-				• Deposits 500.0 rupees into the account.
-				• Withdraws 200.0 rupees from the account.
-				• After each transaction, display the updated account details, including the new balance.
+	 In the main() function, create an array accounts of BankAccount objects. The array contains three elements, each representing a different bank account.
+	 Initialize Each BankAccount object with specific account details such as account number, holder name, and initial balance.
+	 Perform following operations:
+			â€¢ Iterate through each account in the accounts array.
+			â€¢ For each account, display the account details using the display() function.
+			â€¢ Perform Two Transactions:
+			â€¢ Deposits 500.0 rupees into the account.
+			â€¢ Withdraws 200.0 rupees from the account.
+			â€¢ After each transaction, display the updated account details, including the new balance.
 */
 
 #include <iostream>
