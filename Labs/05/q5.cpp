d3468358019206e39bfda22a563de12594026622
Amna <142903458+amnaa26@users.noreply.github.com>
2024-03-09 22:21:34 +0500

    Update q5.cpp

--- Labs/05/q5.cpp
+++ Labs/05/q5.cpp
@@ -3,8 +3,8 @@
  * Desc: Keeping in mind our previous car example, write a class that represents a car, and use aggregation and composition to combine different components like engine, wheels,
          headlights and steering to create the car object.
 
-		      Hint: create the individual classes first before performing the composition. Remember that for aggregation, you will need pointers! You’ll be needing
-		      constructors and setters to set these values in case of aggregation. Same hint applies to other questions.
+	 Hint: create the individual classes first before performing the composition. Remember that for aggregation, you will need pointers! You’ll be needing
+	 constructors and setters to set these values in case of aggregation. Same hint applies to other questions.
 */
 
 
